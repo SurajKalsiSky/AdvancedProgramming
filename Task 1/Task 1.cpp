6979c211b82df1f0caea374c0ad074f852bc588f
Suraj <Suraj.Kalsi@sky.uk>
2020-04-18 22:13:33 +0100

    initialised game class

---
+++ Task 1/Task 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "game.h"
+
+using namespace std;
+
+int main() {
+    cout << "START" << endl;
+    Game game;
+    cout << "END" << endl;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
---
+++ Task 1/game.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Game {
+public:
+	Game() {
+		cout << "Initialise game" << endl;
+	};
+
+
+};
